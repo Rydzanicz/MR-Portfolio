bd90df8b4b7217fe27f12c1a72a07f9985ba73ee
Rydzanicz <asddsfsdfsf@gmail.com>
2025-09-24 19:46:31 +0200

    fixed gcp deploys1

--- portfolio-app/server.js
+++ portfolio-app/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/my-angular-app/browser')));
+app.use(express.static(path.join(__dirname, 'dist/portfolio-app/browser')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/my-angular-app/browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/portfolio-app/browser', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4200;
